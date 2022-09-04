eae6a56e6c7c015a365322c6ac56d0af7b72c61a
yahor shymanchyk <yahor.shymanchyk@instinctools.com>
2022-09-04 18:47:22 +0200

    add top app bar for overview screen

--- app/src/main/java/com/shjman/polygon2/OverviewScreen.kt
+++ app/src/main/java/com/shjman/polygon2/OverviewScreen.kt
@@ -3,9 +3,9 @@ package com.shjman.polygon2
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,39 +25,55 @@ fun OverviewScreen(
         allSpending.value = spentViewModel.getAllSpending()
     }
     val onSpendingClicked = { spending: Spending -> Timber.d("clicked on == $spending") }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Text(
-            text = "Overview screen",
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center
-        )
-        val allSpendingValue = allSpending.value
-        if (allSpendingValue == null) {
+    var isTopAppBarDropdownMenuExpanded by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Overview screen") },
+                actions = {
+                    IconButton(onClick = { isTopAppBarDropdownMenuExpanded = !isTopAppBarDropdownMenuExpanded }) {
+                        Icon(Icons.Default.MoreVert, "")
+                    }
+                    DropdownMenu(
+                        expanded = isTopAppBarDropdownMenuExpanded,
+                        onDismissRequest = { isTopAppBarDropdownMenuExpanded = false }
+                    ) {
+                        DropdownMenuItem(onClick = { /*TODO*/ }) {
+                            Text("Compare monthly spends")
+                        }
+                    }
+                }
+            )
+        },
+        content = {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                CircularProgressIndicator(color = Color.Green)
-            }
-        } else if (allSpendingValue.isEmpty()) {
-            Text(text = "no data / empty")
-        } else {
-            LazyColumn {
+                val allSpendingValue = allSpending.value
+                if (allSpendingValue == null) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator(color = Color.Green)
+                    }
+                } else if (allSpendingValue.isEmpty()) {
+                    Text(text = "no data / empty")
+                } else {
+                    LazyColumn {
 //                itemsIndexed(allSpendingValue) { index, item ->
 //                    SpendingItem(spending = item, onSpendingClicked = onSpendingClicked)
 //                }
-                item {
-                    val currentLocalDateTime = LocalDateTime.now()
-                    val beginOfCurrentMonth = LocalDateTime.of(
-                        currentLocalDateTime.year,
-                        currentLocalDateTime.month,
-                        1,
-                        0,
-                        0
-                    )
+                        item {
+                            val currentLocalDateTime = LocalDateTime.now()
+                            val beginOfCurrentMonth = LocalDateTime.of(
+                                currentLocalDateTime.year,
+                                currentLocalDateTime.month,
+                                1,
+                                0,
+                                0
+                            )
 //                    val beginOfNextMonth = beginOfCurrentMonth.plusMonths(1)
 //                    val lastDayOfCurrentMonth = LocalDateTime.of(
 //                        currentLocalDateTime.year,
@@ -66,44 +82,45 @@ fun OverviewScreen(
 //                        23,
 //                        59
 //                    )
-                    var amountByLastMonth = 0
-                    val allSpendingValueFilteredByLastMonth = allSpendingValue.filter { it.date?.isAfter(beginOfCurrentMonth) == true }
-                    allSpendingValueFilteredByLastMonth.onEach { spending -> spending.spentAmount?.let { amountByLastMonth += it } }
-                    Text(text = "amount spent this month == $amountByLastMonth")
-                    val categories = mutableSetOf<String>()
-                    allSpendingValueFilteredByLastMonth.onEach { spending -> spending.category?.let { it -> categories.add(it) } }
-                    when {
-                        categories.isEmpty() -> Text(text = "categories == empty")
-                        else -> {
-                            val amountsByCategories = StringBuffer()
-                            categories.onEach { categoryString ->
-                                var amountByCategory = 0
-                                allSpendingValueFilteredByLastMonth
-                                    .filter { it.category == categoryString }
-                                    .onEach { spending -> spending.spentAmount?.let { amountByCategory += it } }
-                                amountsByCategories.append("$categoryString == $amountByCategory, ")
+                            var amountByLastMonth = 0
+                            val allSpendingValueFilteredByLastMonth = allSpendingValue.filter { it.date?.isAfter(beginOfCurrentMonth) == true }
+                            allSpendingValueFilteredByLastMonth.onEach { spending -> spending.spentAmount?.let { amountByLastMonth += it } }
+                            Text(text = "amount spent this month == $amountByLastMonth")
+                            val categories = mutableSetOf<String>()
+                            allSpendingValueFilteredByLastMonth.onEach { spending -> spending.category?.let { it -> categories.add(it) } }
+                            when {
+                                categories.isEmpty() -> Text(text = "categories == empty")
+                                else -> {
+                                    val amountsByCategories = StringBuffer()
+                                    categories.onEach { categoryString ->
+                                        var amountByCategory = 0
+                                        allSpendingValueFilteredByLastMonth
+                                            .filter { it.category == categoryString }
+                                            .onEach { spending -> spending.spentAmount?.let { amountByCategory += it } }
+                                        amountsByCategories.append("$categoryString == $amountByCategory, ")
+                                    }
+                                    Text(text = "$amountsByCategories")
+                                }
                             }
-                            Text(text = "$amountsByCategories")
                         }
-                    }
-                }
 
-                allSpendingValue.onEach {
-                    item(
-                        key = it.date.toString(),
-                    ) {
-                        SpendingItem(spending = it, onSpendingClicked = onSpendingClicked)
-                    }
-                }
+                        allSpendingValue.onEach {
+                            item(
+                                key = it.date.toString(),
+                            ) {
+                                SpendingItem(spending = it, onSpendingClicked = onSpendingClicked)
+                            }
+                        }
 //                items(
 //                    items = allSpendingValue,
 //                    itemContent = {
 //                        SpendingItem(spending = it, onSpendingClicked = onSpendingClicked)
 //                    }
 //                )
+                    }
+                }
             }
-        }
-    }
+        })
 }
 
 @Composable
