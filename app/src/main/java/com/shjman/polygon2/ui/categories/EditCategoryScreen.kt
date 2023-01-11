60673f418e2b1ff5b6710706b401beda4bc1bc5d
yahor shymanchyk <yahor.shymanchyk@instinctools.com>
2023-01-11 10:41:38 +0100

    update editCategoryScreen

--- app/src/main/java/com/shjman/polygon2/ui/categories/EditCategoryScreen.kt
+++ app/src/main/java/com/shjman/polygon2/ui/categories/EditCategoryScreen.kt
@@ -2,16 +2,11 @@ package com.shjman.polygon2.ui.categories
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,20 +39,21 @@ fun EditCategoryScreen(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
     ) {
-        TextField(
+        OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
-                .weight(1f),
+                .padding(8.dp),
+            label = { Text(text = "write category") },
             value = category.value,
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
             keyboardActions = KeyboardActions(
                 onDone = { editCategoryViewModel.onDoneClicked() },
             ),
+            shape = RoundedCornerShape(12.dp),
             onValueChange = { editCategoryViewModel.categoryValueChanged(it) },
-            placeholder = { Text("write category") },
         )
 
         Button(
