c06448ad9d3566260c4f92c6a408ba92ee4aa9ed
yahor shymanchyk <yahor.shymanchyk@instinctools.com>
2024-02-20 08:21:37 +0100

    add test with anagrams

--- app/src/androidTest/java/com/shjman/polygon2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/shjman/polygon2/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package com.shjman.polygon2
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +18,37 @@ class ShjmanInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.shjman.polygon2", appContext.packageName)
+        val a1 = checkCountAndPositionOfAnagram(
+            string = "abacab",
+            word = "ab"
+        )
+        val aa1 = arrayOf(0, 1, 4)
+        assertEquals(aa1, a1)
+        val a2 = checkCountAndPositionOfAnagram(
+            string = "abacab",
+            word = "bca"
+        )
+        val aa2 = arrayOf(1, 3)
+        assertEquals(aa2, a2)
+    }
+
+    private fun checkCountAndPositionOfAnagram(
+        string: String,
+        word: String,
+    ): Array<Int> {
+        val result = mutableListOf<Int>()
+        val length = string.length
+        val worldLength = word.length
+        for (i in 0..length - worldLength) {
+            val take = string.substring(i, i + worldLength)
+            val sorted = take.toList().sorted()
+            val sortedWord = word.toList().sorted()
+            val xz = sorted == sortedWord
+            Log.e("aaa", "aa $i take $take sorted $sorted sortedWord $sortedWord xz $xz")
+            if (xz) {
+                result.add(i)
+            }
+        }
+        return result.toTypedArray()
     }
 }
--- app/src/test/java/com/shjman/polygon2/ShjmanUnitTest.kt
+++ app/src/test/java/com/shjman/polygon2/ShjmanUnitTest.kt
@@ -1,8 +1,8 @@
 package com.shjman.polygon2
 
+import android.util.Log
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +12,33 @@ import org.junit.Assert.*
 class ShjmanUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+        val a1 = checkCountAndPositionOfAnagram(
+            string = "abacab",
+            word = "ab"
+        )
+
+        val aa1 = arrayOf(0, 1, 4)
+        assertEquals(aa1, a1)
+
+//        assertEquals(4, 2 + 2)
+    }
+
+    private fun checkCountAndPositionOfAnagram(
+        string: String,
+        word: String,
+    ): Array<Int> {
+        val result = mutableListOf<Int>()
+        result.add(0)
+        result.add(1)
+        result.add(4)
+        val length = string.length
+        for (i in 0..length) {
+            Log.e("aaa", "aa $i")
+
+        }
+
+
+        return result.toTypedArray()
     }
 }
